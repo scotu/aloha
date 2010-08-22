c1e96e5a5a0639c9680351148dad67d06e1218e2
Andreas Aderhold <aderhold.andreas@gmail.com>
2010-08-22 20:12:30 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+import os
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('aloha'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[6:] # Strip "aloha/" or "aloha\"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+setup(name='django-aloha',
+      version='1.0',
+      description='A Django template tag renders a "Hello" in different langauges',
+      author='Andreas Aderhold',
+      url='http://github.com/aha/aloha/',
+      package_dir={'aloha': 'aloha'},
+      packages=packages,
+      package_data={'aloha': data_files},
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
